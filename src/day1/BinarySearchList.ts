f9297b239fcaebb55c3c7bd1401a87c25793d506
Carlos Fernandes <carlos.vinicius.fernandes@gmail.com>
2023-12-19 14:29:56 -0300

    Implemented BinarySearchList

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    /* search(arr, low, high) -> iterate until lo and hi converge */
+    /* requires haystack to be sorted */
 
+    let low = 0
+    let high = haystack.length
+
+    while (low < high) {
+        const i = Math.floor(low +  (high - low)/2)
+        const value = haystack[i]
+        if (value === needle) {
+            return true
+        } else if (value > needle) {
+            high = i
+        } else {
+            low = i + 1
+        }
+    }
+
+    return false
 }
