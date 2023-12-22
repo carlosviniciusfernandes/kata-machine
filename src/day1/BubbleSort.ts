3c39df78e34cbcc6841b787c02910dd5549fea20
Carlos Fernandes <carlos.vinicius.fernandes@gmail.com>
2023-12-21 22:03:17 -0300

    Implemented BubleSort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
-
+    /* Each iteration reduces the number of items we need to look by one => O((N+1)N/2) => O(N²) */
+    for(let i = 0; i < arr.length; ++i) {
+        for(let j = 0; j < arr.length -1 -i; ++j) {
+            if (arr[j] > arr[j+1]) {
+                const aux = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = aux
+            }
+        }
+    }
 }
