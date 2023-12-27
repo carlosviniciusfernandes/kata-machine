17882c82656d0c34c1ca0a987acdd18eb1ad801a
Carlos Fernandes <carlos.vinicius.fernandes@gmail.com>
2023-12-27 09:00:14 -0300

    Added some notes onm bubble sort and binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -7,6 +7,7 @@ export default function bs_list(haystack: number[], needle: number): boolean {
 
     while (low < high) {
         const i = Math.floor(low +  (high - low)/2)
+        // NOTE: by halving it by 2 a number of N times, this produces O(logN) -> log is in base 2
         const value = haystack[i]
         if (value === needle) {
             return true
--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,7 +1,9 @@
 export default function bubble_sort(arr: number[]): void {
-    /* Each iteration reduces the number of items we need to look by one => O((N+1)N/2) => O(N²) */
+    /* Each iteration reduces the number of items we need to look by one  => O((N+1)N/2) => O(N²)
+       TIP: Remember, N + (N-1) + (N-2) ... + (N-N+1) = (N+1) * N/2  => Gauss Sum
+    */
     for(let i = 0; i < arr.length; ++i) {
-        for(let j = 0; j < arr.length -1 -i; ++j) {
+        for(let j = 0; j < arr.length -1 -i; ++j) { // arr.length -1 -i => subtracting 'i' as each iteration, the last item become sorted
             if (arr[j] > arr[j+1]) {
                 const aux = arr[j]
                 arr[j] = arr[j+1]
