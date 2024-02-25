2b2ee7c45225abd53c3d4b30adbdbd3668c1b242
Carlos Fernandes <carlos.vinicius.fernandes@gmail.com>
2024-02-25 20:13:37 -0300

    Implemented BinarySearchTrees with BFS and DFS

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,14 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head]; //pretend it is a queue
 
+    while (q.length) {
+        const curr = q.shift() as BinaryNode<number> //dequeue
+
+        if (curr.value == needle) return true;
+
+        if (curr?.left) q.push(curr.left) //enqueue
+        if (curr?.right) q.push(curr.right) //enqueue
+    }
+
+    return false;
 }
--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,21 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function find(node: BinaryNode<number>, value: number): boolean {
 
+    if (!node) return false;
+
+    if (node.value === value) return true;
+
+    if (node.left && value < node.value) {
+        return find(node.left, value);
+    }
+
+    if (node.right && value > node.value) {
+        return find(node.right, value);
+    }
+
+    return false;
+
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return find(head, needle)
 }
