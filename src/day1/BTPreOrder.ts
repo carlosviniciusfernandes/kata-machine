bbede1f3abca5967703eada3bb581813cd7adf2d
Carlos Fernandes <carlos.vinicius.fernandes@gmail.com>
2024-02-25 14:03:03 -0300

    Implemented BinaryTree traversal

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,17 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    // base case
+    if (!curr) {
+        return path;
+    }
 
+    path.push(curr.value);
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    return path;
+
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
