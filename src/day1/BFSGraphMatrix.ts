1c6480c30af04b6386f128de2b4d9d02d80c6424
Carlos Fernandes <carlos.vinicius.fernandes@gmail.com>
2024-02-26 19:58:33 -0300

    Implemented GraphMatrix and GraphList

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,46 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
 
+    const seen = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
+
+    seen[source] = true;
+    const q: number[] = [source];
+
+    do {
+        const curr = q.shift() as number;
+        if (curr === needle) {
+            break;
+        }
+
+        const adjs = graph[curr]
+        for (let i = 0; i < graph.length; ++i) {
+            if (adjs[i] === 0 || seen[i]) continue;
+
+            seen[i] = true;
+            prev[i] = curr;
+            q.push(i);
+        }
+
+    } while (q.length);
+
+    if (prev[needle] === -1){
+        return null;
+    }
+
+    // build it backwards
+
+    let curr = needle;
+    const output: number[] = [];
+    while (prev[curr] !== -1) {
+        output.push(curr);
+        curr = prev[curr];
+    }
+
+    if (output.length){
+        output.reverse();
+        output.unshift(source);
+        return output;
+    }
+
+    return null;
 }
--- src/day1/Trie.ts
+++ src/day1/Trie.ts
@@ -1,12 +1,21 @@
 export default class Trie {
-    
 
-    
+
+
 
     constructor() {
     }
 
     insert(item: string): void {
+        //curr = head
+        //for c in str
+        // if curr[c]
+        //    curr = curr[c]
+        //  else
+        //    node = createNode()
+        //    curr[c] = node
+        //    curr = node
+        //curr.isWord = true
 
 }
     delete(item: string): void {
